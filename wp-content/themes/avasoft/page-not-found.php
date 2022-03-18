06aa7735b29706b77ceeb76160378d544bf18bfc
Merlene LAWRENCE <merlene.l@avasoft.com>
2022-03-18 15:56:08 +0530

    updated page not found

--- wp-content/themes/avasoft/page-not-found.php
+++ wp-content/themes/avasoft/page-not-found.php
@@ -5,6 +5,7 @@ echo '404 Custom File Invoked';
 ?>
 
 <div class="container">
+    <h1>Testing the merge</h1>
 	<h2>Site is under construction...! We will launch it soon for your visiblity testing github</h2>
 </div>
 
