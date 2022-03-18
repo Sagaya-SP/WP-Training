19b78fd20a84730be957d78e1fb8b1f286f81464
Sagaya Siria Pushpam Anthony Doss <SagayaSiriaPushpam.A@avasoft.com>
2022-03-18 16:10:00 +0530

    added p tag in pnf

--- wp-content/themes/avasoft/page-not-found.php
+++ wp-content/themes/avasoft/page-not-found.php
@@ -7,6 +7,7 @@ echo '404 Custom File Invoked';
 <div class="container">
     <h1>Testing the merge</h1>
 	<h2>Site is under construction...! We will launch it soon for your visiblity</h2>
+	<p> checking merging status</p>
 </div>
 
 <?php
