f6111b80ff1352340e2f7ee1f125069ca3f52fdb
Sagaya Siria Pushpam Anthony Doss <SagayaSiriaPushpam.A@avasoft.com>
2022-03-18 15:53:12 +0530

    Changed page not found page

--- wp-content/themes/avasoft/page-not-found.php
+++ wp-content/themes/avasoft/page-not-found.php
@@ -5,6 +5,7 @@ echo '404 Custom File Invoked';
 ?>
 
 <div class="container">
+	<h3>Sagaya h3 tesing</h3>
 	<h2>Site is under construction...! We will launch it soon for your visiblity testing github</h2>
 </div>
 
